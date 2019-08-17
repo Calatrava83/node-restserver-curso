c2b1446c024cb9f6b6357eadd6616cfc1c0c2d21
Alfredo Calatrava <alfredo.calatrava@gmail.com>
2019-08-17 16:25:03 +0200

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,25 @@
 // =======
-// puerto
+// Puerto
 // =======
 process.env.PORT = process.env.PORT || 3000;
+
+// =======
+// Entorno
+// =======
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// =======
+// Base de datos
+// =======
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://strider:oBiOpZxIifIL414Q@cluster0-dzgjy.mongodb.net/cafe';
+}
+process.env.URLDB = urlDB;
+
+// mongodb: //localhost:27017/cafe
+//     mongodb+srv: //strider:oBiOpZxIifIL414Q@cluster0-dzgjy.mongodb.net/cafe
